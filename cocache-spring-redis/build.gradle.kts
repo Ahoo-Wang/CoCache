bf8f79ffcad1961836dce75b8c3231b8f816d118
Ahoo Wang <ahoowang@qq.com>
2022-05-12 11:02:08 +0800

    add Codecov CI

--- cocache-spring-redis/build.gradle.kts
+++ cocache-spring-redis/build.gradle.kts
@@ -5,5 +5,6 @@ dependencies {
     api("org.springframework.data:spring-data-redis")
     testImplementation("org.springframework.boot:spring-boot-autoconfigure")
     testImplementation("io.lettuce:lettuce-core")
+    testImplementation("me.ahoo.cosid:cosid-core")
     testImplementation("me.ahoo.cosid:cosid-test")
 }
