9e4866d3669adf1b83f85b9218c3faf9776dca60
Ahoo Wang <ahoowang@qq.com>
2022-10-25 16:36:40 +0800

    Improve code coverage: `cocache-spring-boot-starter`

--- cocache-spring-boot-starter/build.gradle.kts
+++ cocache-spring-boot-starter/build.gradle.kts
@@ -6,4 +6,6 @@ dependencies {
     api(project(":cocache-spring-redis"))
     api("org.springframework.boot:spring-boot-starter")
     kapt("org.springframework.boot:spring-boot-configuration-processor:${rootProject.ext.get("springBootVersion")}")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-redis")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
