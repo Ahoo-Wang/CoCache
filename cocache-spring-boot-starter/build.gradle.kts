9a7dd7b6ac02c1611ede4f92e3c9a9f16c5fbdf0
Ahoo Wang <ahoowang@qq.com>
2022-10-27 13:44:52 +0800

    refactor build config

--- cocache-spring-boot-starter/build.gradle.kts
+++ cocache-spring-boot-starter/build.gradle.kts
@@ -3,9 +3,11 @@ plugins {
     kotlin("kapt")
 }
 dependencies {
+    kapt(platform(project(":cocache-dependencies")))
     api(project(":cocache-spring-redis"))
     api("org.springframework.boot:spring-boot-starter")
-    kapt("org.springframework.boot:spring-boot-configuration-processor:2.7.5")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+    kapt("org.springframework.boot:spring-boot-autoconfigure-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-data-redis")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
