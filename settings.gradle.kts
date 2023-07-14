b0e9be3d2221909d32ae28e1c9126f107135ed87
Ahoo Wang <ahoowang@qq.com>
2023-07-14 10:25:51 +0800

    refactor: dependency management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,15 +7,5 @@ include(":cocache-spring-redis")
 include(":cocache-spring-boot-starter")
 include(":cocache-test")
 include(":cocache-example")
-
 include(":code-coverage-report")
 
-pluginManagement {
-    plugins {
-        id("io.gitlab.arturbosch.detekt") version "1.23.0" apply false
-        kotlin("jvm") version "1.9.0" apply false
-        kotlin("plugin.spring") version "1.9.0" apply false
-        id("org.jetbrains.dokka") version "1.8.20" apply false
-        id("io.github.gradle-nexus.publish-plugin") version "1.3.0" apply false
-    }
-}
