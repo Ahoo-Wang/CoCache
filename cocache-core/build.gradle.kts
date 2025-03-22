aeef5ee9ff783361f076ecfceb4fe4a8c255bed8
Ahoo Wang <ahoowang@qq.com>
2025-03-22 22:05:32 +0800

    build(deps): update dependencies and move kotlin-logging-jvm (#326)
    - Move kotlin-logging-jvm from build.gradle.kts to cocache-core/build.gradle.kts
    - Remove slf4j-api from cocache-core dependencies
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- cocache-core/build.gradle.kts
+++ cocache-core/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
     api("me.ahoo.cosid:cosid-core")
     implementation(kotlin("reflect"))
     implementation("com.google.guava:guava")
+    api("io.github.oshai:kotlin-logging-jvm")
     implementation("org.springframework:spring-expression")
     testImplementation(project(":cocache-test"))
 }
