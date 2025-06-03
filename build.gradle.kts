7ae446c8193bfeb768c7159cf33633a28dba1a66
Ahoo Wang <ahoowang@qq.com>
2025-06-03 16:50:49 +0800

    build(deps): add JUnit BOM and platform launcher
    - Add JUnit BOM to test dependencies
    - Include JUnit platform launcher for test runtime
    - Update libs.versions.toml with JUnit version and BOM
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,6 +106,7 @@ configure(libraryProjects) {
     }
     dependencies {
         api(platform(dependenciesProject))
+        testImplementation(platform(rootProject.libs.junit.bom))
         detektPlugins(platform(dependenciesProject))
         implementation("org.slf4j:slf4j-api")
         testImplementation("ch.qos.logback:logback-classic")
@@ -117,6 +118,7 @@ configure(libraryProjects) {
             exclude(group = "org.slf4j", module = "slf4j-api")
         }
         testImplementation("org.junit.jupiter:junit-jupiter-api")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
         detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting")
     }
