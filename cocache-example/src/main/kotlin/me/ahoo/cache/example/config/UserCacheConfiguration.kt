a2b08ca2e0b1f48255e025c8b109e815dfc59859
Ahoo Wang <ahoowang@qq.com>
2025-06-25 12:03:44 +0800

    build(gradle): move detekt configuration to root project (#374)
    - Apply DetektPlugin and configure DetektExtension in root project
    - Remove DetektPlugin application from library projects
    - Update dependencies to use detektPlugins configuration
    - Minor code adjustments in example project files
    
    Signed-off-by: Ahoo Wang <ahoowang@qq.com>

--- cocache-example/src/main/kotlin/me/ahoo/cache/example/config/UserCacheConfiguration.kt
+++ cocache-example/src/main/kotlin/me/ahoo/cache/example/config/UserCacheConfiguration.kt
@@ -13,8 +13,8 @@
 package me.ahoo.cache.example.config
 
 import me.ahoo.cache.annotation.CoCacheMetadata
-import me.ahoo.cache.api.source.CacheSource
 import me.ahoo.cache.api.client.ClientSideCache
+import me.ahoo.cache.api.source.CacheSource
 import me.ahoo.cache.client.MapClientSideCache
 import me.ahoo.cache.example.model.User
 import org.springframework.beans.factory.annotation.Qualifier
